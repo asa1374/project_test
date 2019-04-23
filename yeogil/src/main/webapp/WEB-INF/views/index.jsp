7ceea63b000df8eb0f0e7e17549285bb9daec36b
asa1374 <asa1374@gmail.com>
2019-04-23 16:04:50 +0900

    ing

--- yeogil/src/main/webapp/WEB-INF/views/index.jsp
+++ yeogil/src/main/webapp/WEB-INF/views/index.jsp
@@ -11,10 +11,14 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/area/area.css">
-  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/common/web/default_ko.css?v=150805"/>
-  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/common/web/gnb.css?v=150805" />
-  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/component/reset.css?v=150805" />
-  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/component/default.css?v=150805" />
+  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/area/city_list.css">
+  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/common/web/default_ko.css"/>
+  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/common/web/gnb.css" />
+  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/component/reset.css" />
+  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/component/default.css" />
+  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/city/header_v2.css" />
+  <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/city/spot_list.css" />
+  <%-- <link rel="stylesheet" href="<%=application.getContextPath()%>/resources/css/city/main.css" /> --%>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
 </head>
@@ -24,7 +28,7 @@
 </div>
 <script src="<%=application.getContextPath()%>/resources/js/yeogil.js"></script>
 <script src="<%=application.getContextPath()%>/resources/js/city/city.js"></script>
-<script src="<%=application.getContextPath()%>/resources/js/compo/compo.js"></script>
+<script src="<%=application.getContextPath()%>/resources/js/comp/compo.js"></script>
 <script src="<%=application.getContextPath()%>/resources/js/comm/comm.js"></script>
 <script>
 yeogil.init('<%=application.getContextPath()%>');
--- yeogil/src/main/webapp/resources/js/city/city.js
+++ yeogil/src/main/webapp/resources/js/city/city.js
@@ -13,6 +13,7 @@ city = (()=>{
 	};
 	let setContentView = ()=>{
 		$(compo.common_menu()).appendTo('#common_area');
+		
 		$.each(common_navi(),(i,j)=>{
 			$('<a>'+j.text+'</a>')
 			.appendTo('.common_menu')
@@ -23,42 +24,131 @@ city = (()=>{
 				case 'home':
 					
 					break;
-				case 'hotel':
-					
+				case 'main_city':
+					$.getScript(compojs,()=>{
+						$('.inner_box').remove();
+						$('.white').remove();
+						$('<div id="cj_content"></div>').appendTo('#wrapper');
+						$(compo.tourist_main_city()).appendTo('#cj_content');
+						
+						$('#taipai').click(()=>{
+							$('#cj_content').remove();
+							$('.inner_box').remove();
+							$('.white').remove();
+							$('<div id="cj_content"></div>').appendTo('#wrapper');
+							$(compo.city_detail()).appendTo('#cj_content');
+							$('.area_title b').text('타이베이');
+							$('.area_title span').text('taipai');
+							$('<a> &gt; 타이베이 </a>').addClass('nav_btn').appendTo('.area_nav');
+							$('<img></img>')
+							.attr("src",_+"/resources/img/component/taipai.jpg")
+							.attr("width","628")
+							.attr("height","369")
+							.appendTo('.img_box');
+							
+							city_detail();
+						});
+					});
 					break;
-				case 'touristarea':
-					alert('관광명소 누름');
-					
+				case 'tourist_city':
+					$.getScript(compojs,()=>{
+						$('#cj_content').remove();
+						$('.inner_box').remove();
+						$('.white').remove();
+						$('<div id="cj_content"></div>').appendTo('#wrapper');
+						$(compo.tourist_city()).appendTo('#cj_content');
+						$(compo.tourist_list()).appendTo('#cj_content');
+
+					});
 					break;
 				case 'restaurant':
-					alert('음식점 누름');
+					$.getScript(compojs,()=>{
+						$('#cj_content').remove();
+						$('.inner_box').remove();
+						$('.white').remove();
+						$('<div id="cj_content"></div>').appendTo('#wrapper');
+						$(compo.restaurant_city()).appendTo('#cj_content');
+						$(compo.tourist_list()).appendTo('#cj_content');
+						
+					});
 					break;
 				case 'shopping':
-					alert('쇼핑 누름');
+					$.getScript(compojs,()=>{
+						$('#cj_content').remove();
+						$('.inner_box').remove();
+						$('.white').remove();
+						$('<div id="cj_content"></div>').appendTo('#wrapper');
+						$(compo.shopping_city()).appendTo('#cj_content');
+						$(compo.tourist_list()).appendTo('#cj_content');
+						
+					});
 					break;
 				case 'calendar':
+					
+					break;
+				case 'map':
 					
 					break;
 				}
 			});
 		});
-		city_detail();
+		country_detail();
 	};
 	let common_navi = ()=>{
 	return [
 		{name :"home",text :"홈"},
-		{name :"hotel",text :"호텔"},
-		{name :"touristarea",text :"관광명소"},
+		{name :"main_city",text :"주요도시"},
+		{name :"tourist_city",text :"관광명소"},
 		{name :"restaurant",text :"음식점"},
 		{name :"shopping",text :"쇼핑"},
-		{name :"calendar",text :"여행일정"}
+		{name :"calendar",text :"여행일정"},
+		{name :"map",text :"지도보기"}
 		];
 	};
-	let city_detail = ()=>{
+	let country_detail = ()=>{
+
 		$('<img></img>').attr("src",_+"/resources/img/component/taiwan01.png")
 		.attr("width","625")
 		.attr("height","369").appendTo('.img_box');
+		$('<a class="city_box"><div class="city_title">가오슝</div>'
+				+'<div class="city_desc">가오슝원경관,삼봉궁,보얼예술특구</div>'
+				+'</a>')
+				.appendTo('.city_list');
 		
+		$('<img class="city_img"></img>')
+		.attr("src",_+"/resources/img/component/taiwan02.jpg")
+		.attr("width","346")
+		.attr("height","240")
+		.appendTo('.city_box');
+		
+		$('<a class="city_box"><div class="city_title">타이베이</div>'
+				+'<div class="city_desc">타이베이 기차역 북문~</div>'
+				+'</a>')
+				.appendTo('.city_list');
+		
+		$('<img class="city_img"></img>')
+		.attr("src",_+"/resources/img/component/taiwan03.jpg")
+		.attr("width","346")
+		.attr("height","240")
+		.appendTo('.city_box');
+		
+		$('<a class="city_box"><div class="city_title">화롄</div>'
+				+'<div class="city_desc">바다~</div>'
+				+'</a>')
+				.appendTo('.city_list');
+		
+		$('<img class="city_img"></img>')
+		.attr("src",_+"/resources/img/component/taiwan04.jpg")
+		.attr("width","346")
+		.attr("height","240")
+		.appendTo('.city_box');
+	};
+	let city_detail = ()=>{
+		$('.area_title').text('타이베이 인기장소');
+		
+		$('<img></img>').attr("src",_+"/resources/img/component/taiwan01.png")
+		.attr("width","625")
+		.attr("height","369").appendTo('.img_box');
 		$('<a class="city_box"><div class="city_title">가오슝</div>'
 				+'<div class="city_desc">가오슝원경관,삼봉궁,보얼예술특구</div>'
 				+'</a>')
